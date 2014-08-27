f1347f8c2f2fc1045538576ee6a28463d3d7e534
Brian Hansen <brian.hansen@chartboost.com>
2014-08-26 21:42:42 -0700

    tighten it up a bit

--- process.php
+++ process.php
@@ -4,7 +4,11 @@ $id = $_REQUEST['player_id'];
 
 $url = "https://acs.leagueoflegends.com/v1/stats/player_history/NA/{$id}?begIndex=0&endIndex=20";
 
+// It would be way safer to set more curl options
 $ch = curl_init($url);
 
 $result = curl_exec($ch);
+curl_close($ch);
+
+// Simulate a RESTful response
 echo json_encode($result);
